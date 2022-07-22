8bdc531f74e8dc4656d23756e75cc8716c002256
BISHAL DATTA <94059050+itsMeBishal001@users.noreply.github.com>
2022-07-22 21:08:29 +0530

    Time: 2 ms (87.01%), Space: 48.4 MB (31.42%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,18 +1,11 @@
 class Solution {
     public int[] countBits(int n) {
-        int []arr=new int[n+1];
-        for(int i=0;i<n+1;i++){
-            arr[i]= hammingWeight_03(i);
-        }
-        return arr;
-    }
-    public int hammingWeight_03(int n) {
-        int count = 0;
-        while (n != 0) {
-            count++;
-            n &= (n - 1);
+       
+        int[] ans = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i & (i - 1)] + 1;
         }
 
-        return count;
+        return ans;
     }
 }
