69f3019baea3f14bba1d7fefea03614d3b925a90
BISHAL DATTA <94059050+itsMeBishal001@users.noreply.github.com>
2022-07-23 17:33:01 +0530

    Time: 2 ms (87.01%), Space: 48 MB (57.71%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -4,6 +4,8 @@ class Solution {
         int[] ans = new int[n + 1];
         for (int i = 1; i <= n; i++) {
             ans[i] = ans[i & (i - 1)] + 1;
+                //i&(i*(-i))
+                
         }
 
         return ans;
