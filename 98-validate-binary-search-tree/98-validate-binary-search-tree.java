6e3807a99154d32542daecf4391a777799ab1c4a
BISHAL DATTA <94059050+itsMeBishal001@users.noreply.github.com>
2022-08-21 09:38:14 +0530

    Time: 1 ms (59.01%), Space: 43.8 MB (62.40%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -15,12 +15,8 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-       return morisTraversalInOrder(root);
-    }
-    
-    public boolean morisTraversalInOrder(TreeNode root){
-       // List<Integer> ans=new ArrayList<>();
-        TreeNode curr=root;
+        
+     TreeNode curr=root;
         long prev=-(long)1e13;
         while(curr!=null){
             TreeNode left=curr.left;
@@ -45,6 +41,34 @@ class Solution {
         }
         return true;
     }
+    
+//     public boolean morisTraversalInOrder(TreeNode root){
+//        // List<Integer> ans=new ArrayList<>();
+//         TreeNode curr=root;
+//         long prev=-(long)1e13;
+//         while(curr!=null){
+//             TreeNode left=curr.left;
+//             if(left==null){
+//                 if(prev>=curr.val )return false;
+//                     prev=curr.val;
+//                 // ans.add(curr.val);
+            
+//                 curr=curr.right;
+//             }else{
+//                 TreeNode RMostNode=rightMostNode(left,curr);
+//                 if(RMostNode.right==null){
+//                     RMostNode.right=curr;
+//                     curr=curr.left;
+//                 }else{
+//                     RMostNode.right=null;
+//                     if(prev>=curr.val )return false;
+//                     prev=curr.val;
+//                     curr=curr.right;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
     
     public TreeNode rightMostNode(TreeNode node,TreeNode curr){
         while(node.right!=null&&node.right!=curr){
