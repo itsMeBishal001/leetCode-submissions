36d9faebffece5ae1a86ef35a5a02c1da2285782
BISHAL DATTA <94059050+itsMeBishal001@users.noreply.github.com>
2022-08-22 13:10:32 +0530

    Time: 1 ms (59.01%), Space: 43.5 MB (74.02%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -15,65 +15,23 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        
-     TreeNode curr=root;
+       LinkedList<TreeNode> st=new LinkedList<>();
+        insertAllLeft(root,st);
         long prev=-(long)1e13;
-        while(curr!=null){
-            TreeNode left=curr.left;
-            if(left==null){
-                if(prev>=curr.val )return false;
-                    prev=curr.val;
-                // ans.add(curr.val);
-            
-                curr=curr.right;
-            }else{
-                TreeNode RMostNode=rightMostNode(left,curr);
-                if(RMostNode.right==null){
-                    RMostNode.right=curr;
-                    curr=curr.left;
-                }else{
-                    RMostNode.right=null;
-                    if(prev>=curr.val )return false;
-                    prev=curr.val;
-                    curr=curr.right;
-                }
-            }
+        while(st.size()!=0){
+            TreeNode rnode=st.removeFirst();
+            if(prev>=rnode.val)return false;
+            prev=rnode.val;
+            insertAllLeft(rnode.right,st);
         }
         return true;
     }
-    
-//     public boolean morisTraversalInOrder(TreeNode root){
-//        // List<Integer> ans=new ArrayList<>();
-//         TreeNode curr=root;
-//         long prev=-(long)1e13;
-//         while(curr!=null){
-//             TreeNode left=curr.left;
-//             if(left==null){
-//                 if(prev>=curr.val )return false;
-//                     prev=curr.val;
-//                 // ans.add(curr.val);
-            
-//                 curr=curr.right;
-//             }else{
-//                 TreeNode RMostNode=rightMostNode(left,curr);
-//                 if(RMostNode.right==null){
-//                     RMostNode.right=curr;
-//                     curr=curr.left;
-//                 }else{
-//                     RMostNode.right=null;
-//                     if(prev>=curr.val )return false;
-//                     prev=curr.val;
-//                     curr=curr.right;
-//                 }
-//             }
-//         }
-//         return true;
-//     }
-    
-    public TreeNode rightMostNode(TreeNode node,TreeNode curr){
-        while(node.right!=null&&node.right!=curr){
-            node=node.right;
+    public void insertAllLeft(TreeNode node, LinkedList<TreeNode> st){
+        while(node!=null){
+            st.addFirst(node);
+            node=node.left;
         }
-        return node;
     }
+    
+    
 }
