6caafb05d1939e3a8cdf2ea755462ba72ffc22e2
BISHAL DATTA <94059050+itsMeBishal001@users.noreply.github.com>
2022-08-27 21:32:45 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (6.20%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,6 +1,18 @@
 class Solution {
-    public int fib(int n) {
-        if(n<=1)return n;
-        return fib(n-1)+fib(n-2);
-    }
+    // public int fib(int n) {
+    //     if(n<=1)return n;
+    //     return fib(n-1)+fib(n-2);
+    // }
+    public int fib(int N) {
+		if(N < 2){
+			return N;
+		}
+		int x = 0;
+		int y = 1;
+		for(int i = 2; i <= N; i++){
+			y = x + y;
+			x = y - x;
+		}
+		return y;
+	}
 }
